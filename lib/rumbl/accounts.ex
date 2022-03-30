00b3da480ccd4a7e736b6ef3ed10c61fd2783f13
XXX <XXX@mimiquate.com>
2022-03-30 12:07:08 -0300

    creacion de base de datos, y llamada de datos persistidos

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -1,24 +1,48 @@
 defmodule Rumbl.Accounts do
   @moduledoc """
   The Accounts context
+  Aqui va la logica de accounts
+  Ahora con logica para traer los datos de la base de datos
+  Se puso los datos de manera estatica al principio por no empezar de una con base de datos.
   """
   alias Rumbl.Accounts.User
-
-  def list_users do
-    [
-      %User{id: "1", name: "Jose", username: "josevalim"},
-      %User{id: "2", name: "Bruce", username: "redrapids"},
-      %User{id: "3", name: "Chris", username: "chrismccord"}
-    ]
-  end
+  alias Rumbl.Repo
 
   def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id === id end)
+    Repo.get(User, id)
+  end
+
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
+    Repo.get_by(User, params)
   end
+
+  def list_users do
+    Repo.all(User)
+  end
+
 end
+
+
+
+
+# def list_users do
+#   [
+#     %User{id: "1", name: "Jose", username: "josevalim"},
+#     %User{id: "2", name: "Bruce", username: "redrapids"},
+#     %User{id: "3", name: "Chris", username: "chrismccord"}
+#   ]
+# end
+# Aca era un get user, pero de la coleccion estatica de los users harcodeados
+# def get_user(id) do
+#   Enum.find(list_users(), fn map -> map.id === id end)
+# end
+# traer un usuario segun un parametro de los usuarios hardcodeados mas arriba
+# def get_user_by(params) do
+#   Enum.find(list_users(), fn map ->
+#     Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+#   end)
+# end
--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -1,3 +1,15 @@
+# defmodule Rumbl.Accounts.User do
+#   defstruct [:id, :name, :username]
+# end
+
+# Ahora usamos schema para matchear las cosas en la base de datos,, pero tambien es como un struct para elixir
 defmodule Rumbl.Accounts.User do
-  defstruct [:id, :name, :username]
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do #por defecto lleva id, y gracias a las macros schema y field,, esto es tanto el struct para elixir como la tabla de la base de datos
+    field :name, :string
+    field :username, :string
+    timestamps()
+  end
 end
