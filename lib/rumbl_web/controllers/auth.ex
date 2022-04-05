58255e96195a588a4299be96c8bd412080bbb923
XXX <XXX@mimiquate.com>
2022-04-05 13:59:20 -0300

    before exploding

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -1,21 +1,20 @@
 defmodule RumblWeb.Auth do
   import Plug.Conn
   import Phoenix.Controller
+
   alias RumblWeb.Router.Helpers, as: Routes
 
-  def init(opts) do
-    opts
-  end
+  def init(opts), do: opts
 
   def call(conn, _opts) do
     user_id = get_session(conn, :user_id)
 
     cond do
-      conn.assigns[:current_user] ->
-        conn
+      user = conn.assigns[:current_user] ->
+        put_current_user(conn, user)
 
       user = user_id && Rumbl.Accounts.get_user(user_id) ->
-        assign(conn, :current_user, user)
+        put_current_user(conn, user)
 
       true ->
         assign(conn, :current_user, nil)
@@ -24,14 +23,23 @@ defmodule RumblWeb.Auth do
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 
   def logout(conn) do
     configure_session(conn, drop: true)
   end
+
 
   def authenticate_user(conn, _opts) do
     if conn.assigns.current_user do
